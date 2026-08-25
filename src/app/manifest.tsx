669e96197cbfd02f7a21ca20d804ede42227a823
suss200 <beroych2@gmail.com>
2026-08-25 18:08:08 +0300

    Added manifest to add web app page icon

--- src/app/manifest.tsx
+++ src/app/manifest.tsx
@@ -11,11 +11,6 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: "#0B1110",
         icons: [
             {
-                src: "/icon-192.png",
-                sizes: "192x192",
-                type: "image/png",
-            },
-            {
                 src: "/icon2.png",
                 sizes: "512x512",
                 type: "image/png",
